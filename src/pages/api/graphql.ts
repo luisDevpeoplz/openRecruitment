4b03fa0313f19117196cef43c2794b04bd0ec9e5
Luis Esteban Ramirez <luis.ramirez@devpeoplz.com>
2023-05-25 16:54:28 -0500

    add graphql-yoga

---
+++ src/pages/api/graphql.ts
@@ -0,0 +1,17 @@
+import { createYoga } from 'graphql-yoga'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { schema } from '../../../graphql/schema'
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+export default createYoga<{
+  req: NextApiRequest
+  res: NextApiResponse
+}>({
+  graphqlEndpoint: '/api/graphql',
+  schema: schema as any,
+})
