7a24bfa4b4194c17640cfcdc3e7f10ff1610ec49
Luis Esteban Ramirez <luis.ramirez@devpeoplz.com>
2023-06-02 14:29:57 -0500

    disable cypress videos

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,10 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return config
     },
     baseUrl: 'http://localhost:3000',
+    video: false,
+    defaultCommandTimeout: 10000,
   },
 })
