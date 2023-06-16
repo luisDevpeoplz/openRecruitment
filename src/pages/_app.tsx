74174755a322f73b38de42b961775fb635a6f28f
Luis Esteban Ramirez <luis.ramirez@devpeoplz.com>
2023-06-16 00:12:03 -0500

    commit test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import Layout from '@/components/UI/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: any) {
   return (
     <Layout>
       <Component {...pageProps} />
