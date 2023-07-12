d30f8f97a56901631ee62471b7e01b4bfd738f3d
Luis Esteban Ramirez <luis.ramirez@devpeoplz.com>
2023-07-12 15:34:38 -0500

    Delete unnecessary code

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,4 @@
 export const isAuthenticated = (url: string) => {
-  // const path = window.location.pathname
-  // const auth = path === '/dashboard' ? true : false
   const auth =
     url === '/' || url === '/login' || url === '/signup' ? false : true
   //This function will return true or false depending on whether the user is authenticated or not.
